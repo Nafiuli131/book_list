3d53dce7a8e8971fc6d0572b9876f1091495ef54
Nafiul Islam <nafiul.islam@synesisit.info>
2022-07-02 15:23:13 +0600

    Add existing project files to Git

---
+++ src/main/java/com/example/model/BookList.java
@@ -0,0 +1,19 @@
+package com.example.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Entity
+@Data
+@Table(name = "bookList")
+public class BookList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "book_name")
+    private String BookName;
+    @Column(name = "author_name")
+    private String AuthorName;
+}
